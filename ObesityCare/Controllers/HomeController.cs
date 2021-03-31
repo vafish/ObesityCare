a460c56204e17bf9fe6e49714b2a4d22ebe415a6
HongyuChen <hche293@student.monash.edu>
2021-03-31 18:56:51 +1100

    Update

--- ObesityCare/Controllers/HomeController.cs
+++ ObesityCare/Controllers/HomeController.cs
@@ -9,10 +9,39 @@ using System.IO;
 using System.Text;
 using System.Drawing;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+using Microsoft.Azure.Services.AppAuthentication;
+using System.Threading.Tasks;
 
 namespace ObesityCare.Controllers
 {
 
+    public class SqlAppAuthenticationProvider : SqlAuthenticationProvider
+    {
+        private static readonly AzureServiceTokenProvider _tokenProvider = new AzureServiceTokenProvider();
+
+        /// <summary>
+        /// Acquires an access token for SQL using AzureServiceTokenProvider with the given SQL authentication parameters.
+        /// </summary>
+        /// <param name="parameters">The parameters needed in order to obtain a SQL access token</param>
+        /// <returns></returns>
+        public override async Task<SqlAuthenticationToken> AcquireTokenAsync(SqlAuthenticationParameters parameters)
+        {
+            var authResult = await _tokenProvider.GetAuthenticationResultAsync("https://database.windows.net/").ConfigureAwait(false);
+
+            return new SqlAuthenticationToken(authResult.AccessToken, authResult.ExpiresOn);
+        }
+
+        /// <summary>
+        /// Implements virtual method in SqlAuthenticationProvider. Only Active Directory Interactive Authentication is supported.
+        /// </summary>
+        /// <param name="authenticationMethod">The SQL authentication method to check whether supported</param>
+        /// <returns></returns>
+        public override bool IsSupported(SqlAuthenticationMethod authenticationMethod)
+        {
+            return authenticationMethod == SqlAuthenticationMethod.ActiveDirectoryInteractive;
+        }
+    }
     //public class BasicAuthenticationAttribute : ActionFilterAttribute
     //{
     //    public string BasicRealm { get; set; }
