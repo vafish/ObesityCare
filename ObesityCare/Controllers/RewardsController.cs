1a090a6d155ec5f2ee9ea8d1035375dfc66d9190
HongyuChen <hche293@student.monash.edu>
2021-05-01 19:06:44 +1000

    Rewards

--- ObesityCare/Controllers/RewardsController.cs
+++ ObesityCare/Controllers/RewardsController.cs
@@ -7,133 +7,130 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ObesityCare.Models;
-using Microsoft.AspNet.Identity;
 
 namespace ObesityCare.Controllers
 {
-    public class RecordsController : Controller
+    public class RewardsController : Controller
     {
-        private ObesityCare_dbRecords db = new ObesityCare_dbRecords();
+        private RewardModel db = new RewardModel();
 
-        // GET: Records
-        [Authorize]
+        // GET: Rewards
         public ActionResult Index()
         {
-            //var AllRecord = db.Record;
-
-            //for (int i = 0; i < AllRecord.Remove; i++)
-            //{
-            //    if (!AllRecord[i].UserId.Equals(User.Identity.GetUserId()))
-            //    {
-            //        AllRecord.RemoveAt(i);
-            //    }               
-            //}
-            string id = User.Identity.GetUserId();
-            var records = from r in db.Record select r;
-            records = records.Where(r => r.UserId.Equals(id));
-
-            return View(records.ToList());
+            return View(db.Rewards.ToList());
         }
 
-
-
-
-        // GET: Records/Details/5
+        // GET: Rewards/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Record record = db.Record.Find(id);
-            if (record == null)
+            Reward reward = db.Rewards.Find(id);
+            if (reward == null)
             {
                 return HttpNotFound();
             }
-            return View(record);
+            return View(reward);
+        }
+        // GET: Rewards/Claim/5
+        public ActionResult Claim(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reward reward = db.Rewards.Find(id);
+            if (reward == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reward);
         }
 
-        // GET: Records/Create
+
+        // GET: Rewards/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Records/Create
+        // POST: Rewards/Create
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性；有关
         // 更多详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Location,Duration,Star,UserId")] Record record)
+        public ActionResult Create([Bind(Include = "Id,Name,Cost")] Reward reward)
         {
             if (ModelState.IsValid)
             {
-
-                db.Record.Find(db.Record.Count());
-                record.Id = db.Record.Count() + 5;
-                record.UserId = User.Identity.GetUserId();
-                db.Record.Add(record);
+                db.Rewards.Add(reward);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(record);
+            return View(reward);
         }
 
-        // GET: Records/Edit/5
+        // GET: Rewards/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Record record = db.Record.Find(id);
-            if (record == null)
+            Reward reward = db.Rewards.Find(id);
+            if (reward == null)
             {
                 return HttpNotFound();
             }
-            return View(record);
+            return View(reward);
         }
 
-        // POST: Records/Edit/5
+        // POST: Rewards/Edit/5
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性；有关
         // 更多详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Name,Location,Duration,Star")] Record record)
+        public ActionResult Edit([Bind(Include = "Id,Name,Cost")] Reward reward)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(record).State = EntityState.Modified;
-
+                db.Entry(reward).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(record);
+            return View(reward);
         }
 
-        // GET: Records/Delete/5
+
+
+        // GET: Rewards/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Record record = db.Record.Find(id);
-            if (record == null)
+            Reward reward = db.Rewards.Find(id);
+            if (reward == null)
             {
                 return HttpNotFound();
             }
-            return View(record);
+            return View(reward);
         }
 
-        // POST: Records/Delete/5
+        
+
+
+        // POST: Rewards/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Record record = db.Record.Find(id);
-            db.Record.Remove(record);
+            Reward reward = db.Rewards.Find(id);
+            db.Rewards.Remove(reward);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
---
+++ ObesityCare/Models/RewardModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ObesityCare.Models
+{
+    public class RewardModel : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“RewardModel”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“ObesityCare.Models.RewardModel”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“RewardModel”
+        //连接字符串。
+        public RewardModel()
+            : base("name=RewardModel")
+        {
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+        public virtual DbSet<Reward> Rewards { get; set; }
+    }
+
+    public class Reward
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Cost { get; set; }
+    }
+}
